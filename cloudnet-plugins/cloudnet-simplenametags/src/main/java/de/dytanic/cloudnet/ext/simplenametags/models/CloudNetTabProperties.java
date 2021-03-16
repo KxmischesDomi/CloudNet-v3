8d38d85f9e1ebdec7372d8d3daedfe4e86a862a0
KxmischesDomi <kxmischesdomi@gmail.com>
2021-03-16 21:01:47 +0100

    Added constructor CloudNetTabProperties(UUID) to get the default CloudNetPerms properties

--- cloudnet-plugins/cloudnet-simplenametags/src/main/java/de/dytanic/cloudnet/ext/simplenametags/models/CloudNetTabProperties.java
+++ cloudnet-plugins/cloudnet-simplenametags/src/main/java/de/dytanic/cloudnet/ext/simplenametags/models/CloudNetTabProperties.java
@@ -1,16 +1,23 @@
 package de.dytanic.cloudnet.ext.simplenametags.models;
 
+import de.dytanic.cloudnet.driver.CloudNetDriver;
+import de.dytanic.cloudnet.driver.permission.IPermissionGroup;
+import de.dytanic.cloudnet.driver.permission.IPermissionUser;
+import de.dytanic.cloudnet.driver.permission.PermissionUserGroupInfo;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Optional;
+import java.util.UUID;
 
 public final class CloudNetTabProperties {
 
-	private final String name;
-	private final String prefix;
-	private final String display;
-	private final String color;
-	private final String suffix;
-	private final int sortId;
+	private String name;
+	private String prefix;
+	private String display;
+	private String color;
+	private String suffix;
+	private int sortId;
 
 	public CloudNetTabProperties(@Nonnull String name, @Nullable String prefix, @Nullable String display, @Nullable String color, @Nullable String suffix, int sortId) {
 		this.name = name;
@@ -19,6 +26,19 @@ public final class CloudNetTabProperties {
 		this.color = color;
 		this.suffix = suffix;
 		this.sortId = sortId;
+	}
+
+	public CloudNetTabProperties(@Nonnull UUID uuid) {
+		IPermissionUser user = CloudNetDriver.getInstance().getPermissionManagement().getUser(uuid);
+		if (user == null) return;
+		IPermissionGroup group = CloudNetDriver.getInstance().getPermissionManagement().getHighestPermissionGroup(user);
+
+		this.name = group.getName();
+		this.prefix = group.getPrefix();
+		this.display = group.getDisplay();
+		this.color = group.getColor();
+		this.suffix = group.getSuffix();
+		this.sortId = group.getSortId();
 	}
 
 	public String getName() {
@@ -43,6 +63,30 @@ public final class CloudNetTabProperties {
 
 	public int getSortId() {
 		return sortId;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public void setDisplay(String display) {
+		this.display = display;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+	public void setSortId(int sortId) {
+		this.sortId = sortId;
 	}
 
 	@Override
